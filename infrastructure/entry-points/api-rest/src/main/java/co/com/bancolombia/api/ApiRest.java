a52118b31bb8676a8e1e3eb1566266e879a5879f
maocq <johncarm@bancolombia.com.co>
2024-09-17 10:27:04 -0500

    Virtual threads

--- infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/ApiRest.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/ApiRest.java
@@ -1,20 +1,20 @@
 package co.com.bancolombia.api;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class ApiRest {
-//    private final MyUseCase useCase;
-
 
     @GetMapping(path = "/path")
     public String commandName() {
-//      return useCase.doAction();
+        log.info("Router /path");
         return "Hello World";
     }
 }
